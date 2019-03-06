43a32c1ec4d1e2c7deca17e917c371f35c74ebd0
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-03-05 21:05:34 -0500

    remove cookie and body parser

--- app.js
+++ app.js
@@ -33,7 +33,6 @@ app.get('/healthcheck', (req, res) => {
 
 app.use(logger('dev'))
 app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
