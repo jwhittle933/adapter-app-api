834ac55ff0b548c90cd23ae11bfecf39ab7ca803
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-03-05 20:06:39 -0500

    updates to migrations

--- DataLayer/migration.js
+++ DataLayer/migration.js
@@ -10,54 +10,48 @@ conn.connect(err => {
   console.log(`Connected as: ${conn.threadId}`)
 })
 
-const presenceOfClassrooms = conn.query(`SELECT * FROM classrooms;`)
-const presenceOfDevices = conn.query(`SELECT * FROM devices;`)
-
 // Migrate room data
-if (presenceOfClassrooms._results.length === 0) {
-  rooms.map(room => {
-    conn.query(`
-			INSERT INTO classrooms (
-				building, 
-				roomNumber, 
-				hasHDMI, 
-				hasVGA
-			)
-			VALUES (
-				'${room.building}', 
-				'${room.roomNumber}', 
-				'${room.hasHDMI}', 
-				'${room.hasVGA}'
-			);
-		`)
-  })
-}
+rooms.map(room => {
+  conn.query(`
+		INSERT INTO classrooms (
+			building, 
+			roomNumber, 
+			hasHDMI, 
+			hasVGA
+		)
+		VALUES (
+			'${room.building}', 
+			'${room.roomNumber}', 
+			'${room.hasHDMI}', 
+			'${room.hasVGA}'
+		);
+	`)
+})
 
-// // Migrate device data
-if (presenceOfDevices._results.length === 0) {
-  devices.map(device => {
-    conn.query(`
-			INSERT INTO devices (
-				id, 
-				name, 
-				hasHDMI, 
-				hasVGA, 
-				adapterHDMI, 
-				adapterVGA, 
-				linkHDMI, 
-				linkVGA
-			)
-			VALUES (
-				'${device._id}', 
-				'${device.name}', 
-				'${device.hasHDMI}', 
-				'${device.hasVGA}', 
-				'${device.adapterHDMI}', 
-				'${device.adapterVGA}', 
-				'${device.linkHDMI}', 
-				'${device.linkVGA}'
-			);
-		`)
-  })
-}
+// Migrate device data
+devices.map(device => {
+  conn.query(`
+		INSERT INTO devices (
+			id, 
+			name, 
+			hasHDMI, 
+			hasVGA, 
+			adapterHDMI, 
+			adapterVGA, 
+			linkHDMI, 
+			linkVGA
+		)
+		VALUES (
+			'${device._id}', 
+			'${device.name}', 
+			'${device.hasHDMI}', 
+			'${device.hasVGA}', 
+			'${device.adapterHDMI}', 
+			'${device.adapterVGA}', 
+			'${device.linkHDMI}', 
+			'${device.linkVGA}'
+		);
+	`)
+})
+
 conn.end()
