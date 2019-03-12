739ac60bc13d2dc0ed125baa45f8a3139be0f32f
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-03-12 10:47:35 -0400

    more startup.sh

--- startup.sh
+++ startup.sh
@@ -4,22 +4,24 @@ if [ -e .env ]; then
   echo "\nFound your .env.\n"
   cat .env
   echo "\n"
-    
 else
   echo "You haven't set your environment variables in a .env file.\nWould you like to create one here? y/n"
   read CREATE
   if [ "$CREATE" = "n" ]; then 
-    echo "Ok. In order to use this API, a .env file must exist in the repo.\n Please create one manually."
+    echo "Ok. In order to use the API, a .env file must exist in the root of the repo.\n Please create one manually."
   else 
     touch .env
-    echo "What is your DB_HOST?"
-    read DB_HOST && echo $DB_HOST >> .env
-    echo "What is your DB_USER?"
-    read DB_USER
-    echo $DB_USER >> .env
-    echo "What is your DB_PASSWORD?"
-    read DB_PASSWORD
-    echo $DB_PASSWORD >> .env
+    echo "What is your database hostname?"
+    read DB_HOST && echo "DB_HOST=$DB_HOST" >> .env
+    echo "What is your database username?"
+    read DB_USER && echo "DB_USER=$DB_USER" >> .env
+    echo "What is your database password?"
+    read DB_PASSWORD && echo "DB_PASSWORD=$DB_PASSWORD" >> .env
+    echo "What is the name of your database?"
+    read DATABASE && echo "DATABASE=$DATABASE" >> .env
+    echo "\nCreated .env file:"
+    cat .env
+  fi
 fi
 
 echo "Would you like to start the server in dev or prod"
