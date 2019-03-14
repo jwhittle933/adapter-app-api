da16a74290c0e21edfcab1ad48615e8bc4f23378
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-03-14 14:49:39 -0400

    reset testing suite

---
+++ test/apiControlller.test.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+const apiController = require('../Controllers/apiController')
+
+describe('API Controller test', () => {
+  describe('Buildings Controller', () => {
+    it('Should return Array of Buildings', done => {
+      //   const buildings = apiController.buildingsController()
+      if (true) {
+        done()
+      } else {
+        done(new Error('How did that happen??'))
+      }
+    })
+  })
+})
