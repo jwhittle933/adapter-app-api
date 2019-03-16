a0b12a7036a0c92b572f258f9c5227d56f7fa77a
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-03-16 19:02:42 -0400

    complete frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,17 +3,17 @@
     <div class="routes">
       <h1>Adapter App API</h1>
       <h2>Available Routes</h2>
-      <p v-for="route in routes" :key="route.route">{{ route.route }}</p>
+      <p v-for="route in routes" :key="route.route" @click="setActive(route)">{{ route.route }}</p>
     </div>
     <div class="resp" v-if="infoMenu">
-      <textarea name="return" rows="20" cols="80"></textarea>
+      <textarea name="return" rows="20" cols="80" v-model="returnVal"></textarea>
     </div>
   </div>
 </template>
 
 <script>
-const returnBase = ['norton', 'cooke', 'library', 'carver', 'rankin']
-const returnBuildings = [
+const returnBase = `['norton', 'cooke', 'library', 'carver', 'rankin']`
+const returnBuildings = `[
   {
     building: 'carver',
     roomNumber: 108,
@@ -32,18 +32,18 @@ const returnBuildings = [
     hasHDMI: false,
     hasVGA: true,
   },
-]
+]`
 
-const returnRoom = [
+const returnRoom = `[
   {
     building: 'carver',
     roomNumber: 108,
     hasHDMI: false,
     hasVGA: true,
   },
-]
+]`
 
-const returnDevices = [
+const returnDevices = `[
   {
     id: 'macbook-air-2011-2014',
     name: 'Macbook Air 2011-2014',
@@ -51,10 +51,8 @@ const returnDevices = [
     hasVGA: false,
     adapterHDMI: 'Thunderbolt-HDMI',
     adapterVGA: 'Thunderbolt-VGA',
-    linkHDMI:
-      'https://www.amazon.com/s?k=thunderbolt+to+HDMI+adapter&ref=nb_sb_noss_2',
-    linkVGA:
-      'https://www.amazon.com/s?k=thunderbolt+to+vga+adapter&ref=nb_sb_noss_2',
+    linkHDMI: '...link...',
+    linkVGA: '...link...',
   },
   {
     id: 'macbook-pro-2011-2014',
@@ -63,14 +61,12 @@ const returnDevices = [
     hasVGA: false,
     adapterHDMI: 'Thunderbolt-HDMI',
     adapterVGA: 'Thunderbolt-VGA',
-    linkHDMI:
-      'https://www.amazon.com/s?k=thunderbolt+to+HDMI+adapter&ref=nb_sb_noss_2',
-    linkVGA:
-      'https://www.amazon.com/s?k=thunderbolt+to+vga+adapter&ref=nb_sb_noss_2',
+    linkHDMI: '...link...',
+    linkVGA: '...link...',
   },
-]
+]`
 
-const returnDevice = [
+const returnDevice = `[
   {
     id: 'macbook-air-2011-2014',
     name: 'Macbook Air 2011-2014',
@@ -78,12 +74,10 @@ const returnDevice = [
     hasVGA: false,
     adapterHDMI: 'Thunderbolt-HDMI',
     adapterVGA: 'Thunderbolt-VGA',
-    linkHDMI:
-      'https://www.amazon.com/s?k=thunderbolt+to+HDMI+adapter&ref=nb_sb_noss_2',
-    linkVGA:
-      'https://www.amazon.com/s?k=thunderbolt+to+vga+adapter&ref=nb_sb_noss_2',
+    linkHDMI: '...link...',
+    linkVGA: '...link...',
   },
-]
+]`
 
 export default {
   name: 'App',
@@ -99,7 +93,15 @@ export default {
       ],
       infoMenu: false,
       activeRoute: '',
+      returnVal: '',
     }
+  },
+  methods: {
+    setActive(route) {
+      this.infoMenu = true
+      this.activeRoute = route.route
+      this.returnVal = route.return
+    },
   },
 }
 </script>
@@ -116,11 +118,14 @@ a {
 }
 
 textarea {
+  height: 60vh;
   border-radius: 10px;
   outline: none;
   background-color: #2b3a42;
   color: #eff2e2;
   padding: 1vw;
+  font-family: monospace;
+  font-size: 1em;
 }
 
 p {
