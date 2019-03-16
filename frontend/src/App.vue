dcda6638ddfe0ee875c941525ead743561135061
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-03-16 18:49:52 -0400

    frontend style and logic

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,13 @@
 <template>
-  <div id="app">
-    <div class="info">
-      <div class="routes">
-        <h1>Adapter App API</h1>
-        <h2>Available Routes</h2>
-        <p v-for="route in routes" :key="route.route">{{ route.route }}</p>
-      </div>
+  <div class="info">
+    <div class="routes">
+      <h1>Adapter App API</h1>
+      <h2>Available Routes</h2>
+      <p v-for="route in routes" :key="route.route">{{ route.route }}</p>
     </div>
-    <div class="resp"></div>
+    <div class="resp" v-if="infoMenu">
+      <textarea name="return" rows="20" cols="80"></textarea>
+    </div>
   </div>
 </template>
 
@@ -34,7 +34,7 @@ const returnBuildings = [
   },
 ]
 
-const returnBuildingRoom = [
+const returnRoom = [
   {
     building: 'carver',
     roomNumber: 108,
@@ -43,19 +43,61 @@ const returnBuildingRoom = [
   },
 ]
 
+const returnDevices = [
+  {
+    id: 'macbook-air-2011-2014',
+    name: 'Macbook Air 2011-2014',
+    hasHDMI: false,
+    hasVGA: false,
+    adapterHDMI: 'Thunderbolt-HDMI',
+    adapterVGA: 'Thunderbolt-VGA',
+    linkHDMI:
+      'https://www.amazon.com/s?k=thunderbolt+to+HDMI+adapter&ref=nb_sb_noss_2',
+    linkVGA:
+      'https://www.amazon.com/s?k=thunderbolt+to+vga+adapter&ref=nb_sb_noss_2',
+  },
+  {
+    id: 'macbook-pro-2011-2014',
+    name: 'Macbook/Macbook Pro 2011-2014',
+    hasHDMI: true,
+    hasVGA: false,
+    adapterHDMI: 'Thunderbolt-HDMI',
+    adapterVGA: 'Thunderbolt-VGA',
+    linkHDMI:
+      'https://www.amazon.com/s?k=thunderbolt+to+HDMI+adapter&ref=nb_sb_noss_2',
+    linkVGA:
+      'https://www.amazon.com/s?k=thunderbolt+to+vga+adapter&ref=nb_sb_noss_2',
+  },
+]
+
+const returnDevice = [
+  {
+    id: 'macbook-air-2011-2014',
+    name: 'Macbook Air 2011-2014',
+    hasHDMI: false,
+    hasVGA: false,
+    adapterHDMI: 'Thunderbolt-HDMI',
+    adapterVGA: 'Thunderbolt-VGA',
+    linkHDMI:
+      'https://www.amazon.com/s?k=thunderbolt+to+HDMI+adapter&ref=nb_sb_noss_2',
+    linkVGA:
+      'https://www.amazon.com/s?k=thunderbolt+to+vga+adapter&ref=nb_sb_noss_2',
+  },
+]
+
 export default {
   name: 'App',
   data() {
     return {
-      testField: 'test',
       routes: [
         { route: '/', return: 'The server redirects to this endpoint.' },
         { route: '/buildings', return: returnBase },
         { route: '/buildings/:building', return: returnBuildings },
-        { route: '/buildings/:building/:room', return: '' },
-        { route: '/devices', return: '' },
-        { route: '/devices/:device', return: '' },
+        { route: '/buildings/:building/:room', return: returnRoom },
+        { route: '/devices', return: returnDevices },
+        { route: '/devices/:device', return: returnDevice },
       ],
+      infoMenu: false,
       activeRoute: '',
     }
   },
@@ -73,6 +115,14 @@ a {
   text-decoration: none;
 }
 
+textarea {
+  border-radius: 10px;
+  outline: none;
+  background-color: #2b3a42;
+  color: #eff2e2;
+  padding: 1vw;
+}
+
 p {
   font-family: monospace;
   border: 1px solid grey;
@@ -87,14 +137,14 @@ p:hover {
 }
 .info {
   display: flex;
-  justify-content: space-between;
+  justify-content: space-around;
 }
 .routes {
   margin-left: 3vw;
-  /* margin-top: 3vh; */
   max-width: 20vw;
 }
 .resp {
   width: 50vw;
+  margin-top: 12vh;
 }
 </style>
