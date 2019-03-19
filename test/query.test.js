0fe8cbd638d559eb525639b103aa1f9cca0fb35e
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-03-19 16:59:25 -0400

    begin query tests

--- test/query.test.js
+++ test/query.test.js
@@ -1,17 +1,30 @@
 /* eslint-disable */
 process.env.NODE_ENV = 'test'
 require('dotenv').config()
-const chai = require('chai')
-const chaiHttp = require('chai-http')
-const should = chai.should()
-const apiController = require('../Controllers/apiController')
-const Queries = require('../DataLayer/queries/Queries')
-const RoomList = require('../routes/Data/Classroomlist')
-const Devices = require('../routes/Data/Devicelist')
+const chai = require('chai'),
+  expect = chai.expect,
+  chaiHttp = require('chai-http'),
+  conn = require('../DataLayer/connection'),
+  should = chai.should(),
+  apiController = require('../Controllers/apiController'),
+  Queries = require('../DataLayer/queries/Queries'),
+  RoomList = require('../routes/Data/Classroomlist'),
+  Devices = require('../routes/Data/Devicelist')
+
+const {
+  queryForListOf,
+  queryForBuilding,
+  queryForRoom,
+  queryForDevice,
+} = Queries
 
 describe('>>>>>>>>>>>> Database Query Tests <<<<<<<<<<<<<<<<', () => {
   describe('Query For a List', () => {
     it('Returns list of specified data', done => {
+      conn.query(queryForListOf('building', 'classrooms'), (err, result) => {
+        if (err) done(new Error(err))
+        expect(result).to.not.be.null
+      })
       done()
     })
   })
