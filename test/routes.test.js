bd285166f720a7afa1ca326709e141532bf1f57f
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-03-18 20:46:04 -0400

    more test

--- test/routes.test.js
+++ test/routes.test.js
@@ -63,41 +63,83 @@ describe('Routes Test', () => {
         })
     })
   })
-  const buildings = ['norton', 'carver', 'library', 'cooke', 'rankin']
-  buildings.forEach(building => {
-    describe('GET /api/buildings/norton', () => {
-      it('/api/buildings/norton should respond with Array of room data', done => {
-        chai
-          .request(app)
-          .get(`/api/buildings/${building}`)
-          .end((err, res) => {
-            if (err) done(new Error(err))
-            expect(err).to.be.null // connection successful
-            res.should.have.status(200) // good request'
-            if (building === 'norton') {
-              expect(res.res.text).to.include('11')
-              expect(res.res.text).to.include('101')
-              expect(res.res.text).to.include('hasHDMI')
-              done()
-            } else if (building === 'carver') {
-              expect(res.res.text).to.include('108')
-              expect(res.res.text).to.include('135')
-              done()
-            } else if (building === 'library') {
-              expect(res.res.text).to.include('Crismon')
-              expect(res.res.text).to.include('Mullins')
-              done()
-            } else if (building === 'cooke') {
-              expect(res.res.text).to.include('8')
-              expect(res.res.text).to.include('CCRH')
-              done()
-            } else if (building === 'rankin') {
-              expect(res.res.text).to.include('101')
-              expect(res.res.text).to.include('201')
-              done()
-            }
-          })
-      })
+  describe('GET /api/buildings/norton', () => {
+    it('/api/buildings/norton should respond with Array of room data', done => {
+      chai
+        .request(app)
+        .get(`/api/buildings/norton`)
+        .end((err, res) => {
+          if (err) done(new Error(err))
+          expect(err).to.be.null // connection successful
+          res.should.have.status(200) // good request'
+          expect(res.res.text).to.include('11')
+          expect(res.res.text).to.include('101')
+          done()
+        })
+    })
+  })
+  describe('GET /api/buildings/carver', () => {
+    it('/api/buildings/carver should respond with Array of room data', done => {
+      chai
+        .request(app)
+        .get(`/api/buildings/carver`)
+        .end((err, res) => {
+          if (err) done(new Error(err))
+          expect(err).to.be.null // connection successful
+          res.should.have.status(200) // good request'
+          expect(res.res.text).to.include('108')
+          expect(res.res.text).to.include('135')
+          expect(res.res.text).to.include('hasHDMI')
+          done()
+        })
+    })
+  })
+  describe('GET /api/buildings/library', () => {
+    it('/api/buildings/library should respond with Array of room data', done => {
+      chai
+        .request(app)
+        .get(`/api/buildings/library`)
+        .end((err, res) => {
+          if (err) done(new Error(err))
+          expect(err).to.be.null // connection successful
+          res.should.have.status(200) // good request'
+          expect(res.res.text).to.include('Crismon')
+          expect(res.res.text).to.include('Mullins')
+          expect(res.res.text).to.include('hasHDMI')
+          done()
+        })
+    })
+  })
+  describe('GET /api/buildings/cooke', () => {
+    it('/api/buildings/cooke should respond with Array of room data', done => {
+      chai
+        .request(app)
+        .get(`/api/buildings/cooke`)
+        .end((err, res) => {
+          if (err) done(new Error(err))
+          expect(err).to.be.null // connection successful
+          res.should.have.status(200) // good request'
+          expect(res.res.text).to.include('8')
+          expect(res.res.text).to.include('CCRH')
+          expect(res.res.text).to.include('hasHDMI')
+          done()
+        })
+    })
+  })
+  describe('GET /api/buildings/rankin', () => {
+    it('/api/buildings/rankin should respond with Array of room data', done => {
+      chai
+        .request(app)
+        .get(`/api/buildings/rankin`)
+        .end((err, res) => {
+          if (err) done(new Error(err))
+          expect(err).to.be.null // connection successful
+          res.should.have.status(200) // good request'
+          expect(res.res.text).to.include('101')
+          expect(res.res.text).to.include('201')
+          expect(res.res.text).to.include('hasHDMI')
+          done()
+        })
     })
   })
   describe('GET /api/devices', () => {
@@ -111,6 +153,119 @@ describe('Routes Test', () => {
           res.should.have.status(200) // good request'
           expect(res.res.text).to.include('Apple iPad')
           expect(res.res.text).to.include('Seminary Issue Dell Laptop')
+          expect(res.res.text).to.include('hasHDMI')
+          done()
+        })
+    })
+  })
+  describe('GET /api/devices/apple-ipad', () => {
+    it('/api/devices/:device should respond with Array of a single device objects', done => {
+      chai
+        .request(app)
+        .get(`/api/devices/apple-ipad`)
+        .end((err, res) => {
+          if (err) done(new Error(err))
+          expect(err).to.be.null // connection successful
+          res.should.have.status(200) // good request'
+          expect(res.res.text).to.include('Apple iPad')
+          expect(res.res.text).to.include('hasHDMI')
+          expect(res.res.text).to.include('adapterHDMI')
+          done()
+        })
+    })
+  })
+  describe('GET /api/devices/apple-iphone', () => {
+    it('/api/devices/:device should respond with Array of a single device objects', done => {
+      chai
+        .request(app)
+        .get(`/api/devices/apple-iphone`)
+        .end((err, res) => {
+          if (err) done(new Error(err))
+          expect(err).to.be.null // connection successful
+          res.should.have.status(200) // good request'
+          expect(res.res.text).to.include('Apple iPhone')
+          expect(res.res.text).to.include('hasHDMI')
+          expect(res.res.text).to.include('adapterHDMI')
+          done()
+        })
+    })
+  })
+  describe('GET /api/devices/macbook-2017-present', () => {
+    it('/api/devices/:device should respond with Array of a single device objects', done => {
+      chai
+        .request(app)
+        .get(`/api/devices/macbook-2017-present`)
+        .end((err, res) => {
+          if (err) done(new Error(err))
+          expect(err).to.be.null // connection successful
+          res.should.have.status(200) // good request'
+          expect(res.res.text).to.include('Macbook 2017-present')
+          expect(res.res.text).to.include('hasHDMI')
+          expect(res.res.text).to.include('adapterHDMI')
+          done()
+        })
+    })
+  })
+  describe('GET /api/devices/macbook-air-2011-2014', () => {
+    it('/api/devices/:device should respond with Array of a single device objects', done => {
+      chai
+        .request(app)
+        .get(`/api/devices/macbook-air-2011-2014`)
+        .end((err, res) => {
+          if (err) done(new Error(err))
+          expect(err).to.be.null // connection successful
+          res.should.have.status(200) // good request'
+          expect(res.res.text).to.include('Macbook Air 2011-2014')
+          expect(res.res.text).to.include('hasHDMI')
+          expect(res.res.text).to.include('adapterHDMI')
+          done()
+        })
+    })
+  })
+  describe('GET /api/devices/macbook-pro-2011-2014', () => {
+    it('/api/devices/:device should respond with Array of a single device objects', done => {
+      chai
+        .request(app)
+        .get(`/api/devices/macbook-pro-2011-2014`)
+        .end((err, res) => {
+          if (err) done(new Error(err))
+          expect(err).to.be.null // connection successful
+          res.should.have.status(200) // good request'
+          expect(res.res.text).to.include('Macbook/Macbook Pro 2011-2014')
+          expect(res.res.text).to.include('hasHDMI')
+          expect(res.res.text).to.include('adapterHDMI')
+          done()
+        })
+    })
+  })
+  describe('GET /api/devices/seminary-dell', () => {
+    it('/api/devices/:device should respond with Array of a single device objects', done => {
+      chai
+        .request(app)
+        .get(`/api/devices/seminary-dell`)
+        .end((err, res) => {
+          if (err) done(new Error(err))
+          expect(err).to.be.null // connection successful
+          res.should.have.status(200) // good request'
+          expect(res.res.text).to.include('Seminary Issue Dell Laptop')
+          expect(res.res.text).to.include('hasHDMI')
+          expect(res.res.text).to.include('adapterHDMI')
+          done()
+        })
+    })
+  })
+  describe('GET /api/devices/surface', () => {
+    it('/api/devices/:device should respond with Array of a single device objects', done => {
+      chai
+        .request(app)
+        .get(`/api/devices/surface`)
+        .end((err, res) => {
+          if (err) done(new Error(err))
+          expect(err).to.be.null // connection successful
+          res.should.have.status(200) // good request'
+          expect(res.res.text).to.include('Surface/Surface Pro')
+          expect(res.res.text).to.include('hasHDMI')
+          expect(res.res.text).to.include('adapterHDMI')
           done()
         })
     })
