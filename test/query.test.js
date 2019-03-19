1d8a055a537a8d3cb5b502fdbbc0665153dfac5f
Jonathan Whittle <Jonathan.Whittle@eltoro.com>
2019-03-19 19:32:44 -0400

    more testing

--- test/query.test.js
+++ test/query.test.js
@@ -28,4 +28,42 @@ describe('>>>>>>>>>>>> Database Query Tests <<<<<<<<<<<<<<<<', () => {
       done()
     })
   })
+  describe('Query For a Building', () => {
+    it('Returns an Array of room objects for a specific building', done => {
+      const building = 'norton'
+      conn.query(queryForBuilding(building), (err, result) => {
+        if (err) done(new Error(err))
+        expect(result).to.not.be.null
+        expect(result.length).to.equal(22)
+        expect(result[0].building).to.equal(building)
+      })
+      done()
+    })
+  })
+  describe('Query For a Room', () => {
+    it('Returns an Array with a room object for a specific building', done => {
+      const roomNumber = '11'
+      const building = 'norton'
+      conn.query(queryForRoom(building, roomNumber), (err, result) => {
+        if (err) done(new Error(err))
+        expect(result).to.not.be.null
+        expect(result.length).to.equal(1)
+        expect(result[0].roomNumber).to.equal(roomNumber)
+        expect(result[0].building).to.equal(building)
+      })
+      done()
+    })
+  })
+  describe('Query For a Device', () => {
+    it('Returns an Array with a device object', done => {
+      const id = 'apple-ipad'
+      conn.query(queryForDevice(id), (err, result) => {
+        if (err) done(new Error(err))
+        expect(result).to.not.be.null
+        expect(result.length).to.equal(1)
+        expect(result[0].id).to.equal(id)
+      })
+      done()
+    })
+  })
 })
